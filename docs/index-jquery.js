3325e57d1ba15b44a6d8bbf423f4c8f9399daa6d
kmin93 (Min Kang) <kmin93@gmail.com>
2017-05-21 03:37:15 +0900

    add visual guide

--- docs/index-jquery.js
+++ docs/index-jquery.js
@@ -22,4 +22,29 @@ $("#submit").click(
 			time: date.getTime()
 		});
 	}); 
+
+var modal = document.getElementById('helpModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+document.getElementById('l4').onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = "help-.png";
+    console.log(modalImg.width)
+    modalImg.width = 100%
+    console.log(modalImg.width)
+    // captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
 })
+
