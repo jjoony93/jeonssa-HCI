489241d4492b58ab07e47a9bc3e1d03a2f7448fa
jjoony93 <jjoony93@gmail.com>
2017-05-22 15:17:06 +0900

    firebase+ 메인 input 알람없
    firebase더할때 priority number랑 random code추가함 + 메인페이지 input error 옆에
    빨간글씨로바꿈

--- docs/index-jquery.js
+++ docs/index-jquery.js
@@ -1,3 +1,53 @@
+var reserv_code=["ability","able","aboard","about","above","accept","accident","according",
+  "account","accurate","acres","across","act","action","active","activity",
+  "actual","actually","add","addition","additional","adjective","adult","adventure",
+  "advice","affect","afraid","after","afternoon","again","against","age",
+  "bee","been","before","began","beginning","begun","behavior","behind",
+  "being","believed","bell","belong","below","belt","bend","beneath",
+  "bent","beside","best","bet","better","between","beyond","bicycle",
+  "bigger","biggest","bill","birds","birth","birthday","bit","bite",
+  "city","class","classroom","claws","clay","clean","clear","clearly",
+  "climate","climb","clock","close","closely","closer","cloth","clothes",
+  "clothing","cloud","club","coach","coal","coast","coat","coffee",
+  "cold","collect","college","colony","color","column","combination","combine",
+  "come","comfortable","coming","command","common","community","company","compare",
+  "difference","different","difficult","difficulty","dig","dinner","direct","direction",
+  "directly","dirt","dirty","disappear","discover","discovery","discuss","discussion",
+  "disease","dish","distance","distant","divide","division","do","doctor",
+  "does","dog","doing","doll","dollar","done","donkey","door",
+  "dot","double","doubt","down","dozen","draw","drawn","dream",
+  "dress","drew","dried","drink","drive","driven","driver","driving",
+  "drop","dropped","drove","dry","duck","due","dug","dull",
+  "during","dust","duty","each","eager","ear","earlier","early",
+  "earn","earth","easier","easily","east","easy","eat","eaten",
+  "edge","education","effect","effort","egg","eight","either","electric",
+  "electricity","element","elephant","eleven","else","empty","end","enemy",
+  "energy","engine","engineer","enjoy","enough","enter","entire","entirely",
+  "environment","equal","equally","equator","equipment","escape","especially","essential",
+  "establish","even","evening","event","eventually","ever","every","everybody",
+  "everyone","everything","everywhere","evidence","exact","exactly","examine","example",
+  "excellent","except","exchange","excited","excitement","exciting","exclaimed","exercise",
+  "exist","expect","experience","experiment","explain","explanation","explore","express",
+  "expression","extra","eye","face","facing","fact","factor","factory",
+  "failed","fair","fairly","fall","fallen","familiar","family","famous",
+  "far","farm","farmer","farther","fast","fastened","faster","fat",
+  "father","favorite","fear","feathers","feature","fed","feed","feel",
+  "feet","fell","fellow","felt","fence","few","fewer","field",
+  "fierce","fifteen","fifth","fifty","fight","fighting","figure","fill",
+  "film","final","finally","find","fine","finest","finger","finish",
+  "former","fort","forth","forty","forward","fought","found","four",
+  "fourth","fox","frame","free","freedom","frequently","fresh","friend",
+  "friendly","frighten","frog","from","front","frozen","fruit","fuel",
+  "full","fully","fun","function","funny","fur","furniture","further",
+  "future","gain","game","garage","garden","gas","gasoline","gate",
+  "gather","gave","general","generally","gentle","gently","get","getting",];
+
+var used_code=[];
+var priority_index= 1;
+
+var reserve = reserv_code[Math.floor( Math.random() * reserv_code.length )]
+
+
 $(document).ready(function() {
 
 var config = {
@@ -7,6 +57,23 @@ var config = {
 
 firebase.initializeApp(config);
 
+firebase.database().ref('users').once("value", function(snapshot) {
+    if(snapshot.exists()){
+        var keys= Object.keys(snapshot.val())
+        var lists =snapshot.val()
+    	keys.map(function(key, index) {
+        var list = lists[key]
+            if(list.reservation_code==reserve){
+            	reserve = reserv_code[Math.floor( Math.random() * reserv_code.length )]
+            }
+        used_code.push(list.phone);
+        
+        });
+
+    }
+    priority_index=used_code.length+1
+});
+
 $("#submit").click(
 	function () {
 		var date = new Date();
@@ -15,37 +82,52 @@ $("#submit").click(
 		var persons = $("#persons").val();
 
 		if(!phone && !persons){
-			alert("Please fill out both forms.")
+			$('#phone_warning').html("");
+			$('#phone_warning').html(" * Please fill out both forms.")
+			//alert("Please fill out both forms.")
 			return false
 		}
 		else if (!phone){
-			alert("Please fill in phone number")
+			$('#phone_warning').html("");
+			$('#phone_warning').html(" * Please fill in phone number.")
+			//alert("Please fill in phone number")
 			return false
 		}
 		else if(phone.length!=13){
-			alert("Phone number not filled completely.");
+			$('#phone_warning').html("");
+			$('#phone_warning').html(" * Phone number not filled completely.");
+			//alert("Phone number not filled completely.");
 			return false
 		}
-
 		else if(!persons){
-			alert("Please fill in # of members.")
+			$('#persons_warning').html("");
+			$('#persons_warning').html(" * Please fill in # of members.")
+			//alert("Please fill in # of members.")
 			return false
 		}
-
 		else{
+		
+
+
 		phone = phone.replace(/\-/g,'');
 		// phone = parseInt(phone);
 		persons = parseInt(persons);
-		firebase.database().ref('users').push({
+		firebase.database().ref('users/priority : ' + priority_index.toString()).push({
 			phone: phone,
 			persons: persons,
+			reservation_code: reserve,
 			time: date.getTime()
 		});
+
+		priority_index++;
+		$('#res_code').html(reserve);
+		$('#res_url').html('https://shoutkey.com/'+reserve);
 		$('#phone').val('');
 		$('#phone').attr('placeholder','Phone Number (e.g. 010-1234-5678)').focus().blur();
 
 		$('#persons').val('');
 		$('#persons').attr('placeholder','Number of people in your team (e.g. 3)').focus().blur();
+		reserve=reserv_code[Math.floor( Math.random() * reserv_code.length )]
 		}
 	}); 
 
@@ -91,9 +173,17 @@ $('input[name="phone"]').mask('000-0000-0000');
 
 $('#clear_phone').click(function(e) {
   $('#phone').val('');
+  $('#phone').attr('placeholder','Phone Number (e.g. 010-1234-5678)').focus().blur();
 });
 
 $('#clear_persons').click(function(e) {
   $('#persons').val('');
+  $('#persons').attr('placeholder','Number of people in your team (e.g. 3)').focus().blur();
 });
 
+$('#phone').focus(function() {
+  $('#phone_warning').html("");
+});
+$('#persons').focus(function() {
+  $('#persons_warning').html("");
+});
